8030e6fa5a582a71763348f08bf206be919c68d9
HL <745542144@qq.com>
2021-07-15 00:41:55 +0800

    一个 demo

--- src/main/java/com/hl/springsecurity/config/MyAccessDeniedHandler.java
+++ src/main/java/com/hl/springsecurity/config/MyAccessDeniedHandler.java
@@ -1,4 +1,23 @@
 package com.hl.springsecurity.config;
 
-public class MyAccessDeniedHandler {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@Configuration
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+
+
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write("{status:403,msg:权限不足}");
+    }
 }
--- src/main/java/com/hl/springsecurity/service/MyserviceImpl.java
+++ src/main/java/com/hl/springsecurity/service/MyserviceImpl.java
@@ -1,4 +1,28 @@
 package com.hl.springsecurity.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+
+@Service
 public class MyserviceImpl {
+
+    //登陆成功后springsecurity会放入一个Authentication
+    public boolean hasPermission(HttpServletRequest request, Authentication authentication){
+        String requestURI = request.getRequestURI();
+        System.out.println("--------"+requestURI);
+        Object principal = authentication.getPrincipal();
+        if(principal instanceof UserDetails){
+            UserDetails userDetails=(UserDetails)principal;
+            Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+           return authorities.contains(new SimpleGrantedAuthority(requestURI));
+        }
+        return false;
+    }
+
 }
